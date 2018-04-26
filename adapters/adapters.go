4e297f064e5ae0732ddaa1e6d3b6718c9bd732f0
Nikita <iam@niklucky.com>
2018-04-26 16:21:08 +0300

    (feat) Added KVAdapter and Redis adapter

--- adapters/adapters.go
+++ adapters/adapters.go
@@ -2,6 +2,7 @@ package adapters
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/syndicatedb/vodka/builders"
 )
@@ -15,6 +16,17 @@ type Adapter interface {
 	QueryRow(string) (*sql.Row, error)
 	Query(...interface{}) (*sql.Rows, error)
 	Builder() builders.Builder
+}
+
+/*
+KVAdapter - Key/value adapter intarface for DataServices
+*/
+type KVAdapter interface {
+	Connect() error
+	Get(key string) ([]byte, error)
+	Set(key string, value []byte, expiry time.Duration) error
+	SetJSON(key string, value interface{}, expiry time.Duration) error
+	Del(key string) error
 }
 
 /*
---
+++ adapters/redis.go
@@ -0,0 +1,89 @@
+package adapters
+
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+// Redis - redis DB mapper
+type Redis struct {
+	config Config
+	client *redis.Client
+}
+
+// Connect - connecting to repository
+func (r *Redis) Connect() error {
+	return r.connect()
+}
+
+/*
+Get - getting data by key
+*/
+func (r *Redis) Get(key string) ([]byte, error) {
+	err := r.checkConnection()
+	if err != nil {
+		return nil, err
+	}
+	// TODO: check for redis.Nil: if err == redis.Nil {
+	return r.client.Get(key).Bytes()
+}
+
+/*
+Del - deleting data by key
+*/
+func (r *Redis) Del(key string) error {
+	err := r.checkConnection()
+	if err != nil {
+		return err
+	}
+	return r.client.Del(key).Err()
+}
+
+/*
+Set - setting key with value and expiration time
+*/
+func (r *Redis) Set(key string, value []byte, exp time.Duration) error {
+	err := r.checkConnection()
+	if err != nil {
+		return err
+	}
+	return r.client.Set(key, value, exp).Err()
+}
+
+/*
+SetJSON - setting key with value and expiration time that will be saved as JSON
+*/
+func (r *Redis) SetJSON(key string, value interface{}, exp time.Duration) error {
+	str, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return r.Set(key, str, exp)
+}
+
+func (r *Redis) connect() error {
+	r.client = redis.NewClient(&redis.Options{
+		Addr:     r.getAddr(),
+		Password: r.config.Password, // no password set
+		DB:       0,                 // use default DB
+	})
+	_, err := r.client.Ping().Result()
+	return err
+}
+
+func (r *Redis) checkConnection() error {
+	if r.client == nil {
+		return r.connect()
+	}
+
+	fmt.Printf("Redis connection: %+v\n", r.client)
+	return nil
+}
+
+func (r *Redis) getAddr() string {
+	return r.config.Host + ":" + strconv.Itoa(r.config.Port)
+}
